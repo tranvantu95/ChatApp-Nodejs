d6ad1be32d22b5c6e5c7f0ccac7c8d2a2f40bd98
Tu Tran <tranvantu130295@gmail.com>
2018-11-27 03:53:46 +0700

    add passport

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,7 @@ var User = require("./model/user");
 module.exports = function(app) {
 
     app.get("/", function (req, res) {
+        console.log(req.user);
         res.render("home");
     });
 
@@ -17,13 +18,18 @@ module.exports = function(app) {
     });
 
     //
-    app.post('/login', function (req, res) {
-        User.findOne({username: req.body.username, password: req.body.password}, function (err, user) {
-            if (err) return console.error(err);
-            console.log(user);
-        });
-        res.render("login");
-    });
+    // app.post('/login', function (req, res) {
+    //     User.findOne({username: req.body.username, password: req.body.password}, function (err, user) {
+    //         if (err) return console.error(err);
+    //         console.log(user);
+    //     });
+    //     res.render("login");
+    // });
+
+    app.post('/login', passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
 
     app.post("/register", function (req, res) {
         console.log(req.body);
@@ -36,6 +42,38 @@ module.exports = function(app) {
             if (err) return console.error(err);
         });
         res.render("register");
+    });
+
+    passport.use('local', new LocalStrategy(
+        function(username, password, done) {
+            console.log('authenticate local', username, password);
+            // User.findOne({ username: username }, function (err, user) {
+            //     if (err) { return done(err); }
+            //     if (!user) {
+            //         return done(null, false, { message: 'Incorrect username.' });
+            //     }
+            //     if (!user.validPassword(password)) {
+            //         return done(null, false, { message: 'Incorrect password.' });
+            //     }
+            //     return done(null, user);
+            // });
+
+            return done(null, {username:username});
+        }
+    ));
+
+    passport.serializeUser(function(user, done) {
+        console.log("serializeUser", user);
+        done(null, user.username);
+    });
+
+    passport.deserializeUser(function(name, done) {
+        // User.findById(id, function(err, user) {
+        //     done(err, user);
+        // });
+
+        console.log("deserializeUser", name);
+        done(null, name);
     });
 
 };
--- index.js
+++ index.js
@@ -7,7 +7,14 @@ app.set("views", "./views");
 // lấy thông tin từ form HTML
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+var session = require("express-session");
+app.use(session({ secret: "cats" , resave: true, saveUninitialized: true}));
+
+var passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 let server = require("http").Server(app);
 server.listen(process.env.PORT || 8080);
